44d08c6680859aef2d6eb52ff11d033b4d7277bd
josilob <bjosilo@gmail.com>
2021-05-14 18:59:38 -0400

    wrap, buttons and text of landing portion

--- src/components/Section.js
+++ src/components/Section.js
@@ -8,6 +8,10 @@ function Section() {
 				<h1>Model S</h1>
 				<p>Order Online for Touchless Delivery</p>
 			</ItemText>
+			<ButtonGroup>
+				<LeftBtn>Custom Order</LeftBtn>
+				<RightBtn>Existing Inventory</RightBtn>
+			</ButtonGroup>
 		</Wrap>
 	);
 }
@@ -21,9 +25,34 @@ const Wrap = styled.div`
 	background-size: cover;
 	background-repeat: no-repeat;
 	background-position: center;
+	display: flex;
+	flex-direction: column;
+	justify-content: space-between;
+	align-items: center;
 `;
 
 const ItemText = styled.div`
 	padding-top: 15vh;
 	text-align: center;
 `;
+
+const ButtonGroup = styled.div`
+	display: flex;
+	margin-bottom: 30px;
+`;
+
+const LeftBtn = styled.button`
+	background-color: rgba(23, 26, 32, 0.8);
+	height: 40px;
+	width: 256px;
+	color: #fff;
+	display: flex;
+	justify-content: center;
+	align-items: center;
+	border-radius: 100px;
+	opacity: 0.85;
+	text-transform: uppercase;
+	font-size: 12px;
+	cursor: pointer;
+`;
+const RightBtn = styled(LeftBtn)``;
