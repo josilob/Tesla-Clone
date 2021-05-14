9a0ba0c810b2d676a44934d9981ef15c6c6655e8
josilob <bjosilo@gmail.com>
2021-05-14 18:28:21 -0400

    images folder, landing page components

---
+++ src/components/Home.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import Section from './Section';
+import styled from 'styled-components';
+
+function Home() {
+	return (
+		<Container>
+			<Section />
+		</Container>
+	);
+}
+
+export default Home;
+
+const Container = styled.div`
+	height: 100vh;
+`;
